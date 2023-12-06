a850b30dee584796bb716e958897cf3aa59f9c12
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-12-05 19:58:25 -0600

    debugging

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const routes = require('./controllers');
-const sequelize = require('../config/connection');
+const sequelize = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 
